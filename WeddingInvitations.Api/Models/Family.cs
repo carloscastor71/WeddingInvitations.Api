1a21cb75175b5de896c178d243da09ee367727b2
CARLOS-DACODES\carlos <ext.carlos.castor@pricetravel.com.mx>
2025-10-20 12:03:34 -0600

    cambio en fecha limite

--- WeddingInvitations.Api/Models/Family.cs
+++ WeddingInvitations.Api/Models/Family.cs
@@ -81,7 +81,7 @@ namespace WeddingInvitations.Api.Models
         // Recordatorios
         public DateTime? LastReminderSent { get; set; }
         public int ReminderCount { get; set; } = 0;
-        public DateTime ResponseDeadline { get; set; } = DateTime.SpecifyKind(new DateTime(2025, 10, 20), DateTimeKind.Utc);
+        public DateTime ResponseDeadline { get; set; } = DateTime.SpecifyKind(new DateTime(2025, 10, 31), DateTimeKind.Utc);
 
 
         // Método para generar código único
